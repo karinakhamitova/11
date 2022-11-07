942f3c35b27f9688ff89f8b3eee27e808a8ccfa9
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-07 23:18:16 +0300

    Добавьте файлы проекта.

---
+++ 11_1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+        public Linear (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (b == 0)
+            {
+                return "Х равен 0";
+            }
+            double r = -b/ k;
+            return $"X равен {r:.000}";
+        }
+    }
+   
+}
---
+++ 11_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для решения уравнения вида k*x+b = 0 введите значения коэффициентов k и b:");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear linear = new Linear (k, b);
+            Console.WriteLine(linear.Root());
+            Console.ReadKey(); 
+        }
+    }
+}
